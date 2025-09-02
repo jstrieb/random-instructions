6ab7c7f0c5b5df9c3715e944357c666ae4a66fc6
Jacob Strieb <jstrieb@users.noreply.github.com>
2025-09-01 23:07:18 -0400

    Add Capstone

--- build.zig
+++ build.zig
@@ -16,6 +16,27 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
 
+    // Build Capstone
+    const capstone_dep = b.dependency("capstone", .{});
+    const capstone_cmake = b.addSystemCommand(&.{
+        "cmake",
+        "-B",
+        "build",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCAPSTONE_BUILD_SHARED_LIBS=1",
+    });
+    capstone_cmake.setCwd(capstone_dep.path(""));
+    const capstone_make = b.addSystemCommand(&.{ "cmake", "--build", "build" });
+    capstone_make.setCwd(capstone_dep.path(""));
+    capstone_make.step.dependOn(&capstone_cmake.step);
+    exe.step.dependOn(&capstone_make.step);
+
+    // Add the Capstone lib and include directories so we can import capstone.h
+    // and link against the Capstone shared object or DLL
+    exe.addLibraryPath(capstone_dep.path("build"));
+    exe.linkSystemLibrary("capstone");
+    exe.addIncludePath(capstone_dep.path("include"));
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -28,6 +49,7 @@ pub fn build(b: *std.Build) void {
 
     const exe_tests = b.addTest(.{ .root_module = exe_mod });
     const run_tests = b.addRunArtifact(exe_tests);
+    run_tests.step.dependOn(b.getInstallStep());
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,51 @@
 const std = @import("std");
+const capstone = @cImport({
+    @cInclude("capstone/capstone.h");
+});
 
 var stdout = std.io.getStdOut().writer();
+
+fn Capstone(arch: capstone.cs_arch, mode: c_int) type {
+    return struct {
+        engine: capstone.csh,
+        const Self = @This();
+
+        pub fn init() !Self {
+            var engine: capstone.csh = undefined;
+            if (capstone.cs_open(arch, mode, &engine) != capstone.CS_ERR_OK) {
+                return error.CapstoneInitFailed;
+            }
+            return .{ .engine = engine };
+        }
+
+        pub fn deinit(self: *Self) void {
+            _ = capstone.cs_close(&self.engine);
+        }
+
+        pub fn disassemble(self: Self, b: []const u8) bool {
+            var instructions: [*c]capstone.cs_insn = undefined;
+            const count = capstone.cs_disasm(
+                self.engine,
+                @ptrCast(b),
+                b.len,
+                0,
+                0,
+                &instructions,
+            );
+            defer capstone.cs_free(instructions, count);
+            return count > 0;
+        }
+    };
+}
+
+test "basic disassembly" {
+    var cs: Capstone(capstone.CS_ARCH_ARM, capstone.CS_MODE_THUMB) = try .init();
+    defer cs.deinit();
+
+    try std.testing.expect(cs.disassemble("\xe0\xf9\x4f\x07"));
+    try std.testing.expect(cs.disassemble("\x00\x00"));
+    try std.testing.expect(!cs.disassemble("\x00"));
+}
 
 pub fn main() !void {
     try stdout.print("Testing!\n", .{});
