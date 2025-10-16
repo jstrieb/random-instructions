73cbedf41d9969718535643f841642be424f6d53
Jacob Strieb <jstrieb@users.noreply.github.com>
2025-10-16 18:57:46 -0400

    Don't rebuild capstone unless forced (or nonnative)

--- build.zig
+++ build.zig
@@ -10,6 +10,11 @@ pub fn build(b: *std.Build) !void {
         "static",
         "Whether to link statically instead of dynamically",
     ) orelse false) or target.result.isMuslLibC();
+    const force_rebuild_capstone = b.option(
+        bool,
+        "rebuild-capstone",
+        "Whether to force rebuilding libcapstone",
+    ) orelse false;
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
@@ -27,41 +32,49 @@ pub fn build(b: *std.Build) !void {
 
     // Build Capstone
     const capstone_dep = b.dependency("capstone", .{});
-    const capstone_clean = b.addSystemCommand(&.{
-        "make", "clean", "-j",
-    });
-    capstone_clean.setCwd(capstone_dep.path(""));
-    const target_triple = try target.result.zigTriple(b.allocator);
-    const capstone_make = b.addSystemCommand(&.{
-        "make",
-        "-j",
-        "CAPSTONE_BUILD_CORE_ONLY=yes",
-        "CAPSTONE_STATIC=yes",
-        "CAPSTONE_SHARED=no",
-        "RANLIB=zig ranlib",
-        "AR=zig ar",
-        try std.fmt.allocPrint(
-            b.allocator,
-            "CC=zig cc -target {s}",
-            .{target_triple},
-        ),
-        try std.fmt.allocPrint(
-            b.allocator,
-            "CXX=zig c++ -target {s}",
-            .{target_triple},
-        ),
-        if (target.result.os.tag == .macos)
+    // Note: this will fail if non-native Capstone is built first
+    const libcapstone_exists =
+        if (capstone_dep.path("").getPath3(b, null).access("libcapstone.a", .{}))
+            true
+        else |_|
+            false;
+    if (force_rebuild_capstone or !target.query.isNative() or !libcapstone_exists) {
+        const capstone_clean = b.addSystemCommand(&.{
+            "make", "clean", "-j",
+        });
+        capstone_clean.setCwd(capstone_dep.path(""));
+        const target_triple = try target.result.zigTriple(b.allocator);
+        const capstone_make = b.addSystemCommand(&.{
+            "make",
+            "-j",
+            "CAPSTONE_BUILD_CORE_ONLY=yes",
+            "CAPSTONE_STATIC=yes",
+            "CAPSTONE_SHARED=no",
+            "RANLIB=zig ranlib",
+            "AR=zig ar",
             try std.fmt.allocPrint(
                 b.allocator,
-                "LIBARCHS={s}",
-                .{@tagName(target.result.cpu.arch)},
-            )
-        else
-            "LIBARCHS=",
-    });
-    capstone_make.step.dependOn(&capstone_clean.step);
-    capstone_make.setCwd(capstone_dep.path(""));
-    exe.step.dependOn(&capstone_make.step);
+                "CC=zig cc -target {s}",
+                .{target_triple},
+            ),
+            try std.fmt.allocPrint(
+                b.allocator,
+                "CXX=zig c++ -target {s}",
+                .{target_triple},
+            ),
+            if (target.result.os.tag == .macos)
+                try std.fmt.allocPrint(
+                    b.allocator,
+                    "LIBARCHS={s}",
+                    .{@tagName(target.result.cpu.arch)},
+                )
+            else
+                "LIBARCHS=",
+        });
+        capstone_make.step.dependOn(&capstone_clean.step);
+        capstone_make.setCwd(capstone_dep.path(""));
+        exe.step.dependOn(&capstone_make.step);
+    }
 
     // Add the Capstone lib and include directories so we can import capstone.h
     // and link against the Capstone shared object or DLL
